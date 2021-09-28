7dbf794d67137856ea3d26f43951fbcb15a0b828
Dripcoding <dripcoding@gmail.com>
2021-09-27 21:32:40 -0700

    set up start method in main class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,17 +13,15 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Scene scene = new Scene(loadFXML("primary"), 640, 480);
+
+        stage.setTitle("Word Guesser");
+        stage.centerOnScreen();
+
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
